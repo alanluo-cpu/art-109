89a7bc2aa2b579baeae5fc438bd251051ba20354
alanluo-cpu <alan.luo@sjsu.edu>
2025-10-07 01:04:09 -0700

    stuff

---
+++ tutorials/dom/js/main.js
@@ -0,0 +1,19 @@
+
+console.log("hello hello");
+
+let pageTitle = document.querySelector("#page-title");
+
+
+//Javascript Timeout changes h1 title after 3 seconds
+
+setTimeout(function(){
+    pageTitle.style.color = "pink";
+    // console.log("time out worked!")
+}, 3000)
+
+// click event on header changes background color
+
+document.querySelector("header").onclick = function() {
+    // console.log("clicked header");
+    document.querySelector("body").style.backgroundColor = "black";
+}
