29886dd01bd550f4ec08d344a9bf3798b67fe870
alanluo-cpu <alan.luo@sjsu.edu>
2025-10-07 01:37:46 -0700

    stuff

--- tutorials/dom/js/main.js
+++ tutorials/dom/js/main.js
@@ -6,14 +6,38 @@ let pageTitle = document.querySelector("#page-title");
 
 //Javascript Timeout changes h1 title after 3 seconds
 
-setTimeout(function(){
+/*setTimeout(function(){
     pageTitle.style.color = "pink";
     // console.log("time out worked!")
-}, 3000)
+}, 3000)*/
 
 // click event on header changes background color
 
-document.querySelector("header").onclick = function() {
+/*document.querySelector("header").onclick = function() {
     // console.log("clicked header");
     document.querySelector("body").style.backgroundColor = "black";
-}
+}*/
+
+document.querySelector("#image-0").addEventListener("click", function() {
+    document.querySelector("#image-0").style.visibility = "hidden";
+})
+
+document.querySelector("#image-1").addEventListener("click", function() {
+    document.querySelector("#image-1").style.visibility = "hidden";
+})
+
+document.querySelector("#image-2").addEventListener("click", function() {
+    document.querySelector("#image-2").style.visibility = "hidden";
+})
+
+document.querySelector("#image-3").addEventListener("click", function() {
+    document.querySelector("#image-3").style.visibility = "hidden";
+})
+
+document.querySelector("#image-4").addEventListener("click", function() {
+    document.querySelector("#image-4").style.visibility = "hidden";
+})
+
+document.querySelector("#image-5").addEventListener("click", function() {
+    document.querySelector("#image-5").style.visibility = "hidden";
+})
